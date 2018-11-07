30ab4f41f287f50749a556ec4c59fa30042d9b5a
Muhammad Rofiq <muhammad.rofiq@bhinneka.com>
2018-11-07 13:47:03 +0700

    fix path menu reducer

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import { menu } from './menubar/reducer';
+import { menu } from './menu/reducer';
 import { content } from './content/reducer';
 import { logger } from 'redux-logger';
 
